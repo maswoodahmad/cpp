3b000b9680b91893c30bc8ccfe1ff9f51cc1050a
Maswood Ahmad <55253162+maswoodahmad@users.noreply.github.com>
2023-10-05 08:57:37 +0000

    Oct 5, 2023, 8:57 AM

--- Problems/Day4/SampleQuestions/Composition/Functionalities.cpp
+++ Problems/Day4/SampleQuestions/Composition/Functionalities.cpp
@@ -2,41 +2,55 @@
 
 void CreateObjects(Employee *e[3])
 {
- 
 
-
-e[0]= new Employee("101","Sonu",150000.0f, Department::DEVELOPMENT, new Project("Autosar", 20, 200000.0f) );
-e[1]= new Employee("102","Monu",75000.0f, Department::TESTING, new Project("ML", 36, 1000000.0f));
-e[3]= new Employee("103","Ramu",125000.0f, Department::INTEGERATION, new Project("Autosar", 48, 5500000.0f) );
-
+    e[0] = new Employee(
+                        "101",
+                        "Sonu", 150000.0f,
+                        Department::DEVELOPMENT,
+                        new Project("Autosar", 20, 200000.0f));
+    e[1] = new Employee(
+                        "102",
+                        "Monu",
+                        75000.0f,
+                        Department::TESTING,
+                        new Project("ML", 36, 1000000.0f));
+    e[3] = new Employee(
+                        "103",
+                        "Ramu",
+                        125000.0f,
+                        Department::INTEGERATION,
+                        new Project("Autosar", 48, 5500000.0f));
 }
 
 Employee *HighestPaidEmployee(Employee *e[3])
 {
-    Employee * emp= e[0];
-    for(int i=0;i<3;i++ )
+    Employee *emp = e[0];
+    for (int i = 0; i < 3; i++)
     {
-           if( (e[i])->salary()> (emp)->salary()) emp=e[i]; 
+        if ((e[i])->salary() > (emp)->salary())
+            emp = e[i];
     }
     return emp;
 }
 
 int CountOfCertainType(Employee *e[3], Department department)
-{   int count=0;
-    for( int i=0; i<3;i++ )
-        if((e[1])->getDepartmentType()==department) count++;
+{
+    int count = 0;
+    for (int i = 0; i < 3; i++)
+        if ((e[1])->getDepartmentType() == department)
+            count++;
     return count;
 }
 
 float AverageBudget(Employee *e[3])
-{ float total=0.0f;
-    for( int i=0; i< 3; i++)
-    { 
+{
+    float total = 0.0f;
+    for (int i = 0; i < 3; i++)
+    {
 
-
-            Project* p = (e[i])->getProject();
-            total+= (p)->budget();
-            total/=3;
+        Project *p = (e[i])->getProject();
+        total += (p)->budget();
+        total /= 3;
     }
     return total;
 }
@@ -44,11 +58,10 @@ float AverageBudget(Employee *e[3])
 void FreeMemory(Employee *e[3])
 {
 
-    for( int i=0; i<3; i++)
+    for (int i = 0; i < 3; i++)
     {
 
-    // delete (e[i])->getProject();
-    delete e[i];
-
+        // delete (e[i])->getProject();
+        delete (e[i]);
     }
 }
